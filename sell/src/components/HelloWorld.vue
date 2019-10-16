70d8e305c320b38fd0cfecb3e457d40c7212ee38
ATai2 <1114677814@qq.com>
2019-10-16 22:38:23 +0800

    del origin ws

---
+++ sell/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <v-header/>
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+import header from "./header/header.vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    "v-header": header
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+#app
+  .tab-wrapper
+    position: fixed
+    top: 136px
+    left: 0
+    right: 0
+    bottom: 0
+</style>
